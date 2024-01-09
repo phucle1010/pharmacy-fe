1b770b82887df1dc4c67c3a8627a7aa18d532bc1
@sangquangtryhard <sang7b7b@gmail.com>
2024-01-10 02:23:04 +0700

    update tab_account

--- src/views/account-settings/TabAccount.tsx
+++ src/views/account-settings/TabAccount.tsx
@@ -20,6 +20,7 @@ import Button, { ButtonProps } from '@mui/material/Button'
 
 // ** Icons Imports
 import Close from 'mdi-material-ui/Close'
+import { Magnify } from 'mdi-material-ui'
 
 const ImgStyled = styled('img')(({ theme }) => ({
   width: 120,
@@ -45,6 +46,16 @@ const ResetButtonStyled = styled(Button)<ButtonProps>(({ theme }) => ({
   }
 }))
 
+const ButtonUpdateStyled = styled(Button)<ButtonProps>(({ theme }) => ({
+  marginLeft: theme.spacing(4.5),
+  [theme.breakpoints.down('sm')]: {
+    width: '100%',
+    textAlign: 'center',  
+  }
+}))
+
+
+
 const TabAccount = () => {
   // ** State
   const [openAlert, setOpenAlert] = useState<boolean>(true)
@@ -67,7 +78,7 @@ const TabAccount = () => {
           <Grid item xs={12} sx={{ marginTop: 4.8, marginBottom: 3 }}>
             <Box sx={{ display: 'flex', alignItems: 'center' }}>
               <ImgStyled src={imgSrc} alt='Profile Pic' />
-              <Box>
+              <Box>      
                 <ButtonStyled component='label' variant='contained' htmlFor='account-settings-upload-image'>
                   Upload New Photo
                   <input
@@ -81,6 +92,22 @@ const TabAccount = () => {
                 <ResetButtonStyled color='error' variant='outlined' onClick={() => setImgSrc('/images/avatars/1.png')}>
                   Reset
                 </ResetButtonStyled>
+
+                <ButtonUpdateStyled component='label' variant='contained'>
+                  Update info
+                </ButtonUpdateStyled>
+
+                <Grid marginLeft={130} container spacing={7}>
+                  <Grid marginLeft ='50dp'>
+                    <TextField fullWidth label='Search' placeholder='Search info here' defaultValue=''  />                   
+                  </Grid>
+
+                  <Button marginLeft={3} marginTop={3}>
+                    <Magnify/>
+                  </Button>
+
+                </Grid>
+                
                 <Typography variant='body2' sx={{ marginTop: 5 }}>
                   Allowed PNG or JPEG. Max size of 800K.
                 </Typography>
@@ -89,24 +116,24 @@ const TabAccount = () => {
           </Grid>
 
           <Grid item xs={12} sm={6}>
-            <TextField fullWidth label='Username' placeholder='johnDoe' defaultValue='johnDoe' />
+            <TextField fullWidth label='Username' placeholder='Enter user name' defaultValue='' />
           </Grid>
           <Grid item xs={12} sm={6}>
-            <TextField fullWidth label='Name' placeholder='John Doe' defaultValue='John Doe' />
+            <TextField fullWidth label='Name' placeholder='Enter name' defaultValue='' />
           </Grid>
           <Grid item xs={12} sm={6}>
             <TextField
               fullWidth
               type='email'
               label='Email'
-              placeholder='johnDoe@example.com'
-              defaultValue='johnDoe@example.com'
+              placeholder=' email_name@example.com'
+              defaultValue=''
             />
           </Grid>
           <Grid item xs={12} sm={6}>
             <FormControl fullWidth>
               <InputLabel>Role</InputLabel>
-              <Select label='Role' defaultValue='admin'>
+              <Select label='Role' defaultValue=''>
                 <MenuItem value='admin'>Admin</MenuItem>
                 <MenuItem value='author'>Author</MenuItem>
                 <MenuItem value='editor'>Editor</MenuItem>
@@ -118,7 +145,7 @@ const TabAccount = () => {
           <Grid item xs={12} sm={6}>
             <FormControl fullWidth>
               <InputLabel>Status</InputLabel>
-              <Select label='Status' defaultValue='active'>
+              <Select label='Status' defaultValue=''>
                 <MenuItem value='active'>Active</MenuItem>
                 <MenuItem value='inactive'>Inactive</MenuItem>
                 <MenuItem value='pending'>Pending</MenuItem>
@@ -126,7 +153,7 @@ const TabAccount = () => {
             </FormControl>
           </Grid>
           <Grid item xs={12} sm={6}>
-            <TextField fullWidth label='Company' placeholder='ABC Pvt. Ltd.' defaultValue='ABC Pvt. Ltd.' />
+            <TextField fullWidth label='Phone number' placeholder='Enter phone number' defaultValue='' />
           </Grid>
 
           {openAlert ? (
